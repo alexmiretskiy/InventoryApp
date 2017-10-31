85799e6a20da7040d210db57fd45f452b1f64f09
alexmiretskiy <miretskiy@mail.ru>
2017-10-31 20:07:07 +0200

    feach
    add provider to manifest

--- app/src/main/java/com/example/miret/inventoryapp/data/ItemContract.java
+++ app/src/main/java/com/example/miret/inventoryapp/data/ItemContract.java
@@ -9,7 +9,7 @@ public class ItemContract {
   private ItemContract() {
   }
 
-  public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp";
+  public static final String CONTENT_AUTHORITY = "com.example.miret.inventoryapp";
 
   public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
